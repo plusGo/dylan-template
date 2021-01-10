33f6c69d67abe06cc267280d236d32a69547ac34
say-hai <mhlqian@qq.com>
2021-01-10 11:55:29 +0800

    chore: ts增加source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    mode: 'none',
+    devtool: 'source-map',// 打包出的js文件是否生成map文件（方便浏览器调试）
+    mode: 'production',
     entry: {
         'dylan-template': './src/index.ts',
-        'dylan-template.min': './src/index.ts'
     },
     output: {
         filename: '[name].js',
@@ -24,12 +24,12 @@ module.exports = {
             }
         ]
     },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                include: /\.min\.js$/
-            })
-        ]
-    }
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [
+    //         new TerserPlugin({
+    //             include: /\.min\.js$/
+    //         })
+    //     ]
+    // }
 };
