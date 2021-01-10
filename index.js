3d7ff3ffb1e32d80e3c2bba5ea149123f03834c3
say-hai <mhlqian@qq.com>
2021-01-10 13:27:52 +0800

    chore: root indexjs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV = 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./dist/dylan-template.min.js');
-}else {
+} else {
     module.exports = require('./dist/dylan-template.js');
 }
